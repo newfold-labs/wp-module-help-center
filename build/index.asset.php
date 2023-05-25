52bafcb7d1e96915aba369e40fb8497f3df15cd5
Amartya Gaur <amarkaushik1999@gmail.com>
2023-05-26 01:03:56 +0530

    update dependencies, add module ai as dep

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '996e7425f7bfa0a3f165');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1876b438a02bb7ce3409');
