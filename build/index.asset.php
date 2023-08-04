23bddeb5942795c74858fb2c0ca6ba7c87beba3b
amartya-dev <amarkaushik1999@gmail.com>
2023-08-04 11:01:14 +0530

    fix: update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '7bbe8be8425a8c5d88a2');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '75b4f124e0c4a81c713f');
