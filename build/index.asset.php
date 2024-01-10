9014b60933186d42f898a3c2816ade77e6dd1715
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2024-01-10 19:19:59 +0530

    creating a new build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '47871efacaa34562e3f9');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '14909b5dc8e12284ee9c');
