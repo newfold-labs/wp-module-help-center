0b7894a72133e31d7b9cfebeaa7b39d240c61ad8
Allen Benny <allen.officially@gmail.com>
2024-06-19 10:19:22 +0530

    Better

--- src/components/ResultContent.js
+++ src/components/ResultContent.js
@@ -17,6 +17,9 @@ export const ResultContent = ( {
 	if ( content && content.length > 0 ) {
 		return (
 			<>
+				{ !content.toLowerCase().includes('follow') && (<h4>
+					{ __( 'Follow these steps:', 'wp-module-help-center' ) }
+				</h4>)}
 				<p dangerouslySetInnerHTML={ { __html: content } } />
 				{ showFeedbackSection && content && content.length > 0 && (
 					<Feedback postId={ postId } source={ source } />
