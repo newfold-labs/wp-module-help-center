ad1bd552605697ee588c29df4c2d6be425542da0
Mamatha Rao <mamatha.rao@newfold.com>
2024-08-07 18:48:17 +0530

    Removed react spinner and used existing loader

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -9,7 +9,6 @@ import { SearchResult } from './SearchResult';
 import { ResultContent } from './ResultContent';
 import { Analytics, LocalStorageUtils, CapabilityAPI } from '../utils';
 import Loader from './Loader';
-import { ClipLoader } from 'react-spinners';
 import { __ } from '@wordpress/i18n';
 
 const SearchResults = ( props ) => {
@@ -19,8 +18,7 @@ const SearchResults = ( props ) => {
 	const [ resultContent, setResultContent ] = useState( '' );
 	const [ postId, setPostId ] = useState();
 	const [ source, setSource ] = useState( 'kb' );
-	const [ multiResults, setMultiResults] = useState({});
-	const [loading, setLoading] = useState(false);
+	const [ multiResults, setMultiResults ] = useState( {} );
 
 	const populateSearchResult = ( resultContent, postId, searchInput ) => {
 		const resultContentFormatted = resultContent.replace( /\n/g, '<br />' );
@@ -39,20 +37,53 @@ const SearchResults = ( props ) => {
 
 	const fetchMultiSearchResults = async ( query, brand ) => {
 		try {
-			const response = await fetch('/wp-json/newfold-multi-search/v1/multi_search', {
-			  method: 'POST',
-			  headers: {
-				'Content-Type': 'application/json',
-			  },
-			  body: JSON.stringify({ query, brand }),
-			});
-			if (!response.ok) {
-			  throw new Error('Network response was not ok');
+			const response = await fetch(
+				'/wp-json/newfold-multi-search/v1/multi_search',
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify( { query, brand } ),
+				}
+			);
+			if ( ! response.ok ) {
+				throw new Error( 'Network response was not ok' );
 			}
 			return await response.json();
-		} catch (error) {
-			console.error('Error fetching multi-search results:', error);
+		} catch ( error ) {
+			console.error( 'Error fetching multi-search results:', error );
 			return {};
+		}
+	};
+
+	const fetchInitialData = async () => {
+		try {
+			// Populate the results from local storage if they exist
+			const {
+				content: currentResultContent,
+				postId: currentResultPostId,
+			} = LocalStorageUtils.getResultInfo();
+			if ( currentResultContent ) {
+				setResultContent( currentResultContent );
+			}
+			if ( currentResultPostId ) {
+				setPostId( currentResultPostId );
+			}
+			const savedInput = LocalStorageUtils.getSearchInput();
+			const input = savedInput || ' ';
+			setSearchInput( input );
+			const brand = await CapabilityAPI.getBrand();
+			const multiSearchResults = await fetchMultiSearchResults(
+				input,
+				brand
+			);
+			setMultiResults( {
+				...multiSearchResults,
+				hits: multiSearchResults?.results?.[ 0 ]?.grouped_hits,
+			} );
+		} catch ( error ) {
+			console.error( 'Error fetching initial data:', error );
 		}
 	};
 
@@ -63,33 +94,9 @@ const SearchResults = ( props ) => {
 	}, [ props.refresh ] );
 
 	useEffect( () => {
-		const fetchInitialData = async () => {
-			try {
-				// Populate the results from local storage if they exist
-				const { content: currentResultContent, postId: currentResultPostId } =
-			LocalStorageUtils.getResultInfo();
-				if ( currentResultContent ) {
-					setResultContent( currentResultContent );
-				}
-				if ( currentResultPostId ) {
-					setPostId( currentResultPostId );
-				}
-				const input = LocalStorageUtils.getSearchInput();
-				
-				if ( input ) {
-					setSearchInput( input );
-					const brand = await CapabilityAPI.getBrand();
-					const multiSearchResults = await fetchMultiSearchResults( input, brand );
-					setMultiResults({...multiSearchResults, hits: multiSearchResults?.results?.[0]?.grouped_hits});
-				}
-			} catch (error) {
-				console.error('Error fetching initial data:', error);
-			}
-		};
-		
 		fetchInitialData();
-	 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	 }, [] );
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [] );
 
 	const getResultMatches = ( query, tokensMatched, fieldsMatched ) => {
 		const tokensPerQuery = tokensMatched / query.split( /\s+/ ).length;
@@ -136,22 +143,27 @@ const SearchResults = ( props ) => {
 
 	const debouncedResults = useMemo( () => {
 		return debounce( async ( query ) => {
-			if ( !query || query.length === 0 ) {
-				setMultiResults({});
+			if ( ! query || query.length === 0 ) {
+				setMultiResults( {} );
 				return;
 			}
-			setLoading(true);
+			setIsLoading( true );
 			try {
 				const brand = await CapabilityAPI.getBrand();
-				const multiSearchResults = await fetchMultiSearchResults( query, brand );
-				if(multiSearchResults?.results?.[0]?.grouped_hits) {
-					setMultiResults({...multiSearchResults, hits: multiSearchResults?.results?.[0]?.grouped_hits});
+				const multiSearchResults = await fetchMultiSearchResults(
+					query,
+					brand
+				);
+				if ( multiSearchResults?.results?.[ 0 ]?.grouped_hits ) {
+					setMultiResults( {
+						...multiSearchResults,
+						hits: multiSearchResults?.results?.[ 0 ]?.grouped_hits,
+					} );
 				}
-			} catch (error) {
-				console.error('Error fetching debounced results:', error);
-			}
-			finally {
-				setLoading(false);
+			} catch ( error ) {
+				console.error( 'Error fetching debounced results:', error );
+			} finally {
+				setIsLoading( false );
 			}
 		}, 300 );
 		// eslint-disable-next-line react-hooks/exhaustive-deps
@@ -211,17 +223,19 @@ const SearchResults = ( props ) => {
 					<span>{ searchInput ? searchInput.length : 0 }/144</span>
 				</p>
 			</div>
-			{loading ? (
-				<ClipLoader /> // show loader when loading is true
+			{ isLoading ? (
+				<Loader /> // show loader when loading is true
 			) : (
 				<ResultContent
 					content={ resultContent }
 					noResult={ noResult }
 					postId={ postId }
 					source={ source }
-					showFeedbackSection={! resultContent.includes( 'do not possess the answer' ) }
+					showFeedbackSection={
+						! resultContent.includes( 'do not possess the answer' )
+					}
 				/>
-			)}
+			) }
 			{ multiResults?.hits?.length > 0 && (
 				<p>
 					<b>
