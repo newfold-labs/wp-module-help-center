f049f93d70efb22c78005a0139575dbdd8b900d5
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2024-08-20 15:52:33 +0530

    updated code to render markdown

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '76487f619f8947ed669b');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '85bd21a71ebc930f4591');
