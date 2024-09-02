9237e654b36e7d9943569d1156f01e8c1e910a45
Mamatha Rao <mamatha.rao@newfold.com>
2024-09-02 19:02:21 +0530

    Add multi-search API call fallback if no matching mutisearch result is found.

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -101,37 +101,57 @@ const SearchResults = ( props ) => {
 		return fieldsMatched >= 1 && tokensPerQuery >= 0.99;
 	};
 
-	const getAIResult = async () => {
-		setIsLoading( true );
-		try {
-			// Check if the algolia results are close enough
-			const hits = multiResults?.hits?.[ 0 ]?.hits;
-			const resultMatches =
-				hits?.length > 0
-					? getResultMatches(
-							searchInput,
-							hits[ 0 ]?.text_match_info?.tokens_matched,
-							hits[ 0 ]?.text_match_info?.fields_matched
-					  )
-					: false;
+	const checkAndPopulateResult = ( hits, searchInput ) => {
+		if ( hits?.length > 0 ) {
+			const resultMatches = getResultMatches(
+				searchInput,
+				hits[ 0 ]?.text_match_info?.tokens_matched,
+				hits[ 0 ]?.text_match_info?.fields_matched
+			);
+	
 			if ( resultMatches ) {
 				populateSearchResult(
 					hits[ 0 ].document.post_content,
 					hits[ 0 ].document.post_id,
 					searchInput
 				);
-				return;
+				return true;
 			}
+		}
+		return false;
+	};
+
+	const getAIResult = async () => {
+		setIsLoading( true );
+		setNoResult( false );
+
+		try {
+			// Check existing multiResults
+			let hits = multiResults?.hits?.[ 0 ]?.hits;
+			if ( checkAndPopulateResult( hits, searchInput ) ) return;
+
+			// Make a new multi-search API call if no match is found
+			const brand = await CapabilityAPI.getBrand();
+			const multiSearchResults = await fetchMultiSearchResults( searchInput, brand );
+			hits = multiSearchResults?.results?.[ 0 ]?.grouped_hits?.[ 0 ]?.hits;
+			if ( checkAndPopulateResult( hits, searchInput ) ) return;
+			
+			// If no match is found in both cases, fall back to AI search
 			setSource( 'ai' );
 			const result = await moduleAI.search.getSearchResult(
 				searchInput,
 				'helpcenter'
 			);
-			populateSearchResult(
-				result.result[ 0 ].text,
-				result.post_id,
-				searchInput
-			);
+			if( result.result[ 0 ] ) {
+				populateSearchResult(
+					result.result[ 0 ].text,
+					result.post_id,
+					searchInput
+				);
+			} else {
+				setNoResult( true );
+			}
+			
 		} catch ( exception ) {
 			setNoResult( true );
 		} finally {
