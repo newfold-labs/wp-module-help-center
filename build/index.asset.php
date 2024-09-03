bc70ec8602e1bea9c60cc1b0d7a0bab3915b680b
Mamatha Rao <mamatha.rao@newfold.com>
2024-09-03 18:03:54 +0530

    comititng build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '9f695924420aef9082a5');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '89a0107d71ae47cc1a5c');
