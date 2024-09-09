5b33299d5fc5612d194cc3e1d39421e6328637ac
ajayadav09 <ajayadav09@gmail.com>
2024-09-09 11:21:36 +0530

    Updated the UI according to new changes

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -2,6 +2,8 @@ import { ReactComponent as SearchIcon } from '../icons/search.svg';
 import { __ } from '@wordpress/i18n';
 import { ThreeDots } from 'react-loader-spinner'; // Assuming you're using this loader
 import Loader from './Loader'; // Importing your Loader component
+import { ReactComponent as PhoneIcon } from '../icons/phone.svg';
+import { ReactComponent as ChatIcon } from '../icons/chat-bubble.svg';
 
 const SearchInput = ( {
 	searchInput,
@@ -14,75 +16,126 @@ const SearchInput = ( {
 	loading, // Added this prop for loading state
 } ) => {
 	return (
-		<div className="search-container__wrapper">
-			{ isLoading ? (
-				// Show the Loader component when isLoading is true
-				<Loader />
-			) : (
-				<>
-					<div className="search-container">
-						<>
-							<button
-								onClick={ () => {
-									document
-										.getElementById( 'search-input-box' )
-										.focus();
-								} }
+		<div className="helpcenter-input-wrapper">
+			<div className="search-container__wrapper">
+				{ isLoading ? (
+					// Show the Loader component when isLoading is true
+					<Loader />
+				) : (
+					<>
+						<div className="search-container">
+							<>
+								<button
+									onClick={ () => {
+										document
+											.getElementById(
+												'search-input-box'
+											)
+											.focus();
+									} }
+								>
+									<SearchIcon />
+								</button>
+								<input
+									type="text"
+									id="search-input-box"
+									style={ { flexGrow: 2 } }
+									value={ searchInput }
+									maxLength="144"
+									placeholder={ __(
+										'Ask me anything…',
+										'wp-module-help-center'
+									) }
+									onChange={ ( e ) => {
+										setSearchInput( e.target.value );
+										populateSearchResult(
+											'',
+											undefined,
+											e.target.value
+										);
+										setNoResult( false );
+										debouncedResults( e.target.value );
+									} }
+									onKeyDown={ async ( e ) => {
+										if ( e.key === 'Enter' ) {
+											await getAIResult();
+										}
+									} }
+								/>
+							</>
+						</div>
+					</>
+				) }
+				{ true && (
+					<div className="attribute">
+						<p>
+							<span>
+								{ searchInput ? searchInput.length : 0 }/144
+							</span>
+						</p>
+					</div>
+				) }
+				{ loading && (
+					<div className="multisearch-loader">
+						<ThreeDots
+							height="40"
+							width="40"
+							radius="4"
+							color="#196BDE"
+							ariaLabel="three-dots-loading"
+							wrapperStyle={ {} }
+							visible={ true }
+						/>
+					</div>
+				) }
+			</div>
+			<div className="helpcenter-supportinfo__wrapper">
+				<div className="">
+					<h4>
+						{ __( 'Account Support', 'wp-module-help-center' ) }
+					</h4>
+					<div className="helpcenter-supportinfo__text">
+						{ __(
+							'If you need help with your Bluehost account, contact our support team:',
+							'wp-module-help-center'
+						) }
+					</div>
+					<div className="helpcenter-supportinfo__telephone">
+						<span>
+							<PhoneIcon />
+						</span>
+						<span>
+							<a
+								href="tel:8884014678"
+								aria-label="Call 888-401-4678"
 							>
-								<SearchIcon />
-							</button>
-							<input
-								type="text"
-								id="search-input-box"
-								style={ { flexGrow: 2 } }
-								value={ searchInput }
-								maxLength="144"
-								placeholder={ __(
-									'Ask me anything…',
+								888-401-4678
+							</a>
+						</span>
+					</div>
+					<div className="helpcenter-supportinfo__chat">
+						<span>
+							<ChatIcon />
+						</span>
+						<span>
+							<a
+								href="https://www.bluehost.com/contact"
+								aria-label={ __(
+									'Chat with support',
 									'wp-module-help-center'
 								) }
-								onChange={ ( e ) => {
-									setSearchInput( e.target.value );
-									populateSearchResult(
-										'',
-										undefined,
-										e.target.value
-									);
-									setNoResult( false );
-									debouncedResults( e.target.value );
-								} }
-								onKeyDown={ async ( e ) => {
-									if ( e.key === 'Enter' ) {
-										await getAIResult();
-									}
-								} }
-							/>
-						</>
-					</div>
-				</>
-			) }
-			{ ! isLoading && (
-				<div className="attribute">
-					<p>
-						<span>
-							{ searchInput ? searchInput.length : 0 }/144
+								target="_blank"
+								rel="noreferrer"
+							>
+								{ __(
+									'Chat with support',
+									'wp-module-help-center'
+								) }
+							</a>
 						</span>
-					</p>
+					</div>
 				</div>
-			) }
-			{ loading && (
-				<div className="multisearch-loader">
-					<ThreeDots
-						height="40"
-						width="40"
-						radius="4"
-						color="#196BDE"
-						ariaLabel="three-dots-loading"
-						wrapperStyle={ {} }
-						visible={ true }
-					/>
-				</div>
-			) }
+			</div>
 		</div>
 	);
 };
