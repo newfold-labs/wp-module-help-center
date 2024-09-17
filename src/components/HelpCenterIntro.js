cb5a95c7843978596b7454711ba0e37d111bc7b1
ajayadav09 <ajayadav09@gmail.com>
2024-09-17 11:53:02 +0530

    updated intro condition

--- src/components/HelpCenterIntro.js
+++ src/components/HelpCenterIntro.js
@@ -1,42 +1,17 @@
 import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { ReactComponent as AIStars } from '../icons/ai-stars.svg';
-import { useRevealText } from '../utils';
+import { useRevealText, LocalStorageUtils } from '../utils';
 
 const HelpCenterIntro = () => {
 	const [ startReveal, setStartReveal ] = useState( false );
 
 	useEffect( () => {
-		const element = document.getElementById( 'nfd-help-center' );
+		const storedResults = LocalStorageUtils.getResultInfo();
 
-		const checkAndSetReveal = () => {
-			if ( element && element.classList.contains( 'help-container' ) ) {
-				setStartReveal( true );
-			}
-		};
-
-		checkAndSetReveal(); // Initial check
-
-		// Set up MutationObserver to watch for class changes
-		// eslint-disable-next-line no-undef
-		const observer = new MutationObserver( ( mutationsList ) => {
-			for ( const mutation of mutationsList ) {
-				if ( mutation.attributeName === 'class' ) {
-					checkAndSetReveal();
-				}
-			}
-		} );
-
-		if ( element ) {
-			observer.observe( element, { attributes: true } );
+		if ( storedResults.length <= 0 ) {
+			setStartReveal( true );
 		}
-
-		// Cleanup the observer on component unmount
-		return () => {
-			if ( element ) {
-				observer.disconnect();
-			}
-		};
 	}, [] );
 
 	const introText = __(
@@ -44,7 +19,10 @@ const HelpCenterIntro = () => {
 		'wp-module-help-center'
 	);
 
-	const revealedIntro = useRevealText( startReveal ? introText : '', 50 );
+	const revealedIntro = useRevealText(
+		startReveal ? introText : introText,
+		50
+	);
 
 	return (
 		<div className="helpcenter-intro">
