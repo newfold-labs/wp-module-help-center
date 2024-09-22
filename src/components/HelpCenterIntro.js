3886e7632f89dac122b38930a87e25016d9ea0c2
ajayadav09 <ajayadav09@gmail.com>
2024-09-23 00:01:46 +0530

    updated intro text

--- src/components/HelpCenterIntro.js
+++ src/components/HelpCenterIntro.js
@@ -22,12 +22,16 @@ const HelpCenterIntro = () => {
 	}, [] );
 
 	const introText = __(
-		'all the </br></br>  answers just yet.',
+		'Hi! I’m your WordPress AI assistant. </br></br> Ask me how to do things in WordPress and I’ll provide step by step instructions.</br></br> I’m still learning so I don’t have all the answers just yet.',
 		'wp-module-help-center'
 	);
 
-	const revealedIntro = useRevealText( introText, 50, startReveal );
-
+	// const revealedIntro = useRevealText( introText, 50, startReveal );
+	const { displayedText: revealedIntro } = useRevealText(
+		introText || '',
+		50,
+		startReveal
+	);
 	return (
 		<div className="helpcenter-intro">
 			<div>
