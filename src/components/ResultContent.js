ef58238de8b5899837f53182cc360457ba293f9d
ajayadav09 <ajayadav09@gmail.com>
2024-09-24 18:34:54 +0530

    loading to top

--- src/components/ResultContent.js
+++ src/components/ResultContent.js
@@ -1,7 +1,6 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import Feedback from './Feedback';
 import NoResults from './NoResults';
-import { useEffect, useState, useRef } from '@wordpress/element';
+import { useEffect, useState, useRef, useMemo } from '@wordpress/element';
 import { useRevealText, LocalStorageUtils } from '../utils';
 import { ReactComponent as UserAvatar } from '../icons/user-avatar.svg';
 import { ReactComponent as AIStars } from '../icons/ai-stars.svg';
@@ -21,61 +20,76 @@ export const ResultContent = ( {
 	isNewResult,
 	searchInput,
 } ) => {
-	const storedResultsLength = LocalStorageUtils.getResultInfo().length;
-
-	const isNewEntry = isNewResult && index === storedResultsLength - 1;
+	const isNewEntry =
+		isNewResult && index === LocalStorageUtils.getResultInfo().length - 1;
+	const responseRef = useRef( null );
 
 	const [ shouldReveal, setShouldReveal ] = useState( false );
-	const responseRef = useRef( null );
 
 	useEffect( () => {
 		if ( ( isNewEntry && responseRef.current ) || isLoading ) {
-			const viewportHeight = window.innerHeight;
-			const minHeight = viewportHeight - 332;
-			responseRef.current.style.minHeight = `${ minHeight }px`;
-			const helpcenterResultsWrapper = document.getElementById(
-				'helpcenterResultsWrapper'
-			);
-			const scrollDistance = helpcenterResultsWrapper.scrollHeight;
-			helpcenterResultsWrapper.scrollBy( {
-				top: scrollDistance,
-				left: 0,
-				behavior: 'smooth',
-			} );
-
-			setShouldReveal( true );
+			adjustHeightAndScroll();
 		}
 	}, [ isNewEntry, isLoading ] );
 
-	const { displayedText: textToDisplay, isComplete: revealComplete } =
-		useRevealText( content || '', 50, shouldReveal && isNewEntry );
+	const adjustHeightAndScroll = () => {
+		const viewportHeight = window.innerHeight;
+		const minHeight = viewportHeight - 332;
+		responseRef.current.style.minHeight = `${ minHeight }px`;
 
-	const MarkdownRenderer = ( { markdownText } ) => {
-		const [ htmlContent, setHtmlContent ] = useState( '' );
-
-		useEffect( () => {
-			const convertedHTML = marked( markdownText );
-			setHtmlContent( convertedHTML );
-		}, [ markdownText ] );
-
-		return (
-			<p
-				className="helpcenter-results"
-				dangerouslySetInnerHTML={ { __html: htmlContent } }
-			/>
+		const helpcenterResultsWrapper = document.getElementById(
+			'helpcenterResultsWrapper'
 		);
+		const scrollDistance = helpcenterResultsWrapper.scrollHeight;
+		helpcenterResultsWrapper.scrollBy( {
+			top: scrollDistance,
+			left: 0,
+			behavior: 'smooth',
+		} );
+
+		setShouldReveal( true );
 	};
+
+	const { displayedText: textToDisplay, isComplete: revealComplete } =
+		useRevealText( content || '', 50, shouldReveal );
+
+	const htmlContent = useMemo(
+		() => marked( textToDisplay ),
+		[ textToDisplay ]
+	);
+
+	function renderContentOrLoading() {
+		if (
+			isLoading &&
+			loadingQuery === questionBlock &&
+			loadingIndex === index &&
+			source === 'ai'
+		) {
+			return <div className="loading-cursor"></div>;
+		} else if ( content && content.length > 0 ) {
+			return (
+				<p
+					className="helpcenter-results"
+					dangerouslySetInnerHTML={ { __html: htmlContent } }
+				/>
+			);
+		}
+		return null;
+	}
+
+	function shouldShowFeedback() {
+		return (
+			! noResult &&
+			showFeedbackSection &&
+			content &&
+			revealComplete &&
+			content.length > 0
+		);
+	}
 
 	return (
 		<>
-			<div
-				ref={ responseRef }
-				className={
-					isNewEntry
-						? 'helpcenter-response-block last-block'
-						: 'helpcenter-response-block'
-				}
-			>
+			<div ref={ responseRef } className="helpcenter-response-block">
 				<div className="helpcenter-question-block">
 					<div className="helpcenter-question__user-avatar">
 						<UserAvatar />
@@ -89,38 +103,16 @@ export const ResultContent = ( {
 						<AIStars />
 					</div>
 					<div>
-						{ /* Show NoResults only for the most recent entry with no results */ }
 						{ noResult && isNewEntry ? (
 							<NoResults />
 						) : (
-							<>
-								{ /* Only show "Loading" for the most recent query at the correct index */ }
-								{ isLoading &&
-								loadingQuery === questionBlock &&
-								loadingIndex === index &&
-								source === 'ai' ? (
-									<div className="loading-cursor"></div>
-								) : (
-									content &&
-									content.length > 0 && (
-										<>
-											<MarkdownRenderer
-												markdownText={ textToDisplay }
-											/>
-										</>
-									)
-								) }
-							</>
+							renderContentOrLoading()
 						) }
 					</div>
 				</div>
-				{ ! noResult &&
-					showFeedbackSection &&
-					content &&
-					revealComplete &&
-					content.length > 0 && (
-						<Feedback postId={ postId } source={ source } />
-					) }
+				{ shouldShowFeedback() && (
+					<Feedback postId={ postId } source={ source } />
+				) }
 			</div>
 		</>
 	);
