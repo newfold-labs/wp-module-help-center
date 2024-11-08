cabd5a74015108cbbd7713c8525b268fc04eab8d
ajayadav09 <ajayadav09@gmail.com>
2024-11-08 20:34:44 +0530

    Disabled helpcenter feature and hidden the ui on onboarding

--- includes/HelpCenterFeatureHooks.php
+++ includes/HelpCenterFeatureHooks.php
@@ -2,6 +2,7 @@
 namespace NewfoldLabs\WP\Module\HelpCenter;
 
 use NewfoldLabs\WP\Module\Data\SiteCapabilities;
+use NewfoldLabs\WP\Module\Features\Features;
 
 /**
  * This class adds helpCenter feature hooks.
@@ -27,7 +28,36 @@ class HelpCenterFeatureHooks {
 	 */
 	public function hooks() {
 		// add filter so we don't show/load help during onboarding
-		add_filter( 'newfold/features/filter/isEnabled:helpCenter', array( $this, 'filterValue' ) );
+		add_filter( 'newfold/features/filter/isEnabled:helpCenter', array( $this, 'filterValue' ));
+		add_filter( 'newfold/features/action/onDisable:helpCenter', array( $this, 'clearhelpCenter' ));
+	}
+
+	/**
+	 * Enqueue inline script as back up to hide the helpcenter UI on onboarding flow 
+	 */
+	public function clearhelpCenter() {
+		add_action('admin_enqueue_scripts', array($this, 'enqueue_clear_storage_script'), 20);
+	}
+	
+	/**
+	 * incline script to clear localStorage
+	 */
+	public function enqueue_clear_storage_script() {
+		
+		$clear_storage_js = <<<JS
+			document.addEventListener("DOMContentLoaded", function() {
+				const nfdHelpContainer = document.getElementById( 'nfd-help-center' );
+				nfdHelpContainer.classList.toggle( 'help-container', false );
+			});
+		JS;
+	
+		// Add the inline script to `nfd-help-center` if it’s enqueued
+		if (wp_script_is('nfd-help-center', 'enqueued')) {
+			wp_add_inline_script('nfd-help-center', $clear_storage_js);
+		} else {
+			// Fallback: Add the inline script directly to `admin_enqueue_scripts`
+			echo "<script>{$clear_storage_js}</script>";
+		}
 	}
 
 	/**
@@ -37,8 +67,16 @@ class HelpCenterFeatureHooks {
 	 * @return boolean the filtered value
 	 */
 	public function filterValue( $value ) {
+		$helpCenterFeature = Features::getInstance()->getFeature('helpCenter');
 		if ( $this->shouldDisable() ) {
 			$value = false;
+			if ($helpCenterFeature) {
+				$helpCenterFeature->disable();
+			}
+		} else {
+			if ($helpCenterFeature) {
+				$helpCenterFeature->enable();
+			}
 		}
 		return $value;
 	}
