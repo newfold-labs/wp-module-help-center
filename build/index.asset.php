292d657f3ed8e96d74902844074585e0e064f20a
ajayadav09 <ajayadav09@gmail.com>
2024-11-29 17:45:17 +0530

    commiting build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '868ae9d5607045463515');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'cbd602aa372baa7bb416');
