9e20434756935685f805b4b7dd2582fd39757993
ajayadav09 <ajayadav09@gmail.com>
2025-02-06 14:49:26 +0530

    commit build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '879a76c20c2140765e3a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'bd1f5b24ee916b590429');
