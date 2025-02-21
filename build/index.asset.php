eb193658bf9dd37951a7ab0b0890652d9f3399cc
Evan Mullins <evanrm@gmail.com>
2025-02-21 18:33:20 -0500

    refactor some i18n

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'f032c348783b7743b18d');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '1478cd9e38ff544555c3');
