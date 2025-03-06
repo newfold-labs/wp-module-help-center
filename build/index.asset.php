fc69548bac9b304ab700d7c78934cb4cc11864f1
ajayadav09 <ajayadav09@gmail.com>
2025-03-06 19:56:04 +0530

    empty input to get default suggestions
    Added an empty string so that the query is not null in the initial call for multisearch suggestions and the user gets the suggestions as the helpcenter is opened.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '1506d30cd20535172ad5');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8ecf3b7a8622111919c7');
