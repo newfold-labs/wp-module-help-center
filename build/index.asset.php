5c43960b5b73896a36722440b95932deaf036285
ajayadav09 <ajayadav09@gmail.com>
2025-04-18 10:33:40 +0530

    build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8ecf3b7a8622111919c7');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '689a9a8f421f33b46e0d');
