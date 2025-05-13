6989ee66e85768c935249aae0473f1c03994848f
emanuela.castorina <emanuela.castorina@yithemes.com>
2025-05-13 15:14:09 +0200

    padding fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd502aaf4c0f9ceb2f47f');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd5be1c92463c754a261b');
