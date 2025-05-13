11d0f61e9198626fac7c3417adc5cf61bf2f0cc5
ajayadav09 <ajayadav09@gmail.com>
2025-05-13 19:35:04 +0530

    aligned input box button

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd5be1c92463c754a261b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'bec81f07de49ccad1afc');
