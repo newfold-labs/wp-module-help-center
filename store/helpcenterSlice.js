5d3cc3d62483ddfdcfe823d7298920cde307062d
ajayadav09 <ajayadav09@gmail.com>
2025-05-26 17:51:46 +0530

    update

--- store/helpcenterSlice.js
+++ store/helpcenterSlice.js
@@ -19,22 +19,22 @@ const initialState = {
 	triggerSearch: false,
 };
 
-const helpcenterSlice = createSlice({
+const helpcenterSlice = createSlice( {
 	name: 'helpcenter',
 	initialState,
 	reducers: {
-		initialDataSet: (state, action) => {
+		initialDataSet: ( state, action ) => {
 			state.isFooterVisible = action.payload.isFooterVisible;
 			state.searchInput = action.payload.SearchInput;
 		},
-		updateHelpResultHistoryFromDB: (state, action) => {
+		updateHelpResultHistoryFromDB: ( state, action ) => {
 			state.helpResultHistory = action.payload;
 		},
-		updateHelpResultHistory: (state, action) => {
+		updateHelpResultHistory: ( state, action ) => {
 			if (
-				!action.payload ||
+				! action.payload ||
 				typeof action.payload !== 'object' ||
-				Array.isArray(action.payload)
+				Array.isArray( action.payload )
 			) {
 				console.warn(
 					'Skipped pushing to helpResultHistory: Invalid payload',
@@ -42,65 +42,89 @@ const helpcenterSlice = createSlice({
 				);
 				return;
 			}
-			if (state.helpResultHistory.length === 3) {
+			if ( state.helpResultHistory.length === 3 ) {
 				state.helpResultHistory.shift();
 			}
-			state.helpResultHistory.push(action.payload);
+			state.helpResultHistory.push( action.payload );
 		},
-		setDisliked: (state, action) => {
+		setDisliked: ( state, action ) => {
 			state.disliked = action.payload;
 		},
-		setIsFooterVisible: (state, action) => {
+		setIsFooterVisible: ( state, action ) => {
 			state.isFooterVisible = action.payload;
 		},
-		setNoResult: (state) => {
+		setNoResult: ( state ) => {
 			state.noResult = true;
 		},
-		updateHelpEnabled: (state, action) => {
+		updateHelpEnabled: ( state, action ) => {
 			state.helpEnabled = action.payload;
 		},
-		updateVisibility: (state, action) => {
+		updateVisibility: ( state, action ) => {
 			state.visible = action.payload;
 		},
-		updateResultContent: (state, action) => {
+		updateResultContent: ( state, action ) => {
 			state.resultContent = action.payload;
 		},
-		setNewSearchResult: (state, action) => {
+		setNewSearchResult: ( state, action ) => {
 			state.isNewResult = action.payload;
 			state.searchInput = '';
 		},
-		updateMultiResults: (state, action) => {
+		updateMultiResults: ( state, action ) => {
 			state.multiResults = action.payload.results;
 			state.showSuggestions = action.payload.suggestions;
 		},
-		updateInitComplete: (state, action) => {
+		updateInitComplete: ( state, action ) => {
 			state.initComplete = action.payload;
 		},
-		updateSearchInput: (state, action) => {
+		updateSearchInput: ( state, action ) => {
 			state.noResult = false;
 			state.errorMsg = '';
 			state.searchInput = action.payload;
 		},
-		searchInputCatch: (state) => {
+		searchInputCatch: ( state ) => {
 			state.noResult = true;
 			state.isNewResult = true;
 		},
-		searchInputFinally: (state) => {
+		searchInputFinally: ( state ) => {
 			state.searchInput = '';
 			state.isLoading = false;
 			state.loadingIndex = null;
 			state.showSuggestions = false;
 		},
-		setAIResultLoading: (state) => {
+		setAIResultLoading: ( state ) => {
 			state.isLoading = true;
 			state.showSuggestions = false;
 			state.loadingQuery = state.searchInput;
 		},
-		setTriggerSearch: (state, action) => {
+		setTriggerSearch: ( state, action ) => {
 			state.triggerSearch = action.payload;
 		},
+		goBackInHistory: ( state ) => {
+			if ( state.helpResultHistory.length > 0 ) {
+				const previous =
+					state.helpResultHistory[
+						state.helpResultHistory.length - 1
+					];
+
+				// Clone the history (immutably)
+				state.helpResultHistory = [
+					...state.helpResultHistory.slice( 0, -1 ),
+				];
+				console.log( 'PREVIOuS::', previous.resultContent );
+				// Set new state from a shallow copy
+				state.resultContent = Array.isArray( previous.resultContent )
+					? [ ...previous.resultContent ]
+					: [];
+
+				state.searchInput = previous.searchInput || '';
+				state.multiResults = previous.multiResults || {};
+				state.initComplete = true;
+				state.isLoading = false;
+				state.showSuggestions = true;
+			}
+		},
 	},
-});
+} );
 
 export const helpcenterActions = helpcenterSlice.actions;
 
