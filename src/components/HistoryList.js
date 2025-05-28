dfabbd66052d52f4ce8dc6276480680c831e5e9c
manikantakailasa <manikanta.kailasa@gmail.com>
2025-05-28 16:37:30 +0530

    back button from history list fix

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -6,13 +6,14 @@ const HistoryList = () => {
   const dispatch = useDispatch();
   const { helpResultHistory } = useSelector((state) => state.helpcenter);
   const handleHistory = (index) => {
+    dispatch(helpcenterActions.clearViaLinkSearch());
     dispatch(helpcenterActions.setIsFooterVisible(false));
     dispatch(
       helpcenterActions.updateResultContent(helpResultHistory[index])
     );
   };
 
-  return helpResultHistory.map((history, index) => (
+  return [...helpResultHistory].reverse().map((history, index) => (
     <div className="HistoryList">
       <HistoryIcon />
       <div key={index} onClick={() => handleHistory(index)}>
