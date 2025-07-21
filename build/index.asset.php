d2fa16fb64ae39c7d7857ffd25cad230cde5e6c1
Ajay Yadav <ajayadav09@gmail.com>
2025-07-22 02:46:44 +0530

    build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'cc3fe0815cea97ec7aa0');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8ca4c327d66a48771911');
