1d46739dd4d3a52bff59908726bfd0e518ffbad7
Emanuela Antonina Castorina <emanuela@kream.it>
2025-12-09 12:20:01 +0100

    fix li margin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '0a54733cad6deb89305c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c2d38d581c3153d1a21c');
