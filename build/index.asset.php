49aab282e976999102befeaff84d9c00e92e8109
Emanuela Antonina Castorina <emanuela@kream.it>
2026-02-06 11:53:31 +0100

    fix title margin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'e2df6e455ac403fac81d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '427d6dcf19972240b232');
