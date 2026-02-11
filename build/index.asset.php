8ff46c57883a2185ef6760b67dda735a489e2ea4
arunshenoy99 <devarunshenoy99@gmail.com>
2026-02-12 00:09:24 +0530

    Build and styles: update assets, remove unused icons, style updates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '02b5380a2d8450ae87e7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '83b19170b099f0ef3f0c');
