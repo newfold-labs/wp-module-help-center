9d7671968b546550adce9613cdaeb593f8c5050d
arunshenoy99 <devarunshenoy99@gmail.com>
2026-02-23 20:28:27 +0530

    chore: update build artifacts for Newfold agents chat

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '05ef44f93d22d4f562e1');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '23934df5bdd2cf3c541c');
