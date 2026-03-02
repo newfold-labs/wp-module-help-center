a268938a16fceb874fc73322c9a755033a3c11a3
arunshenoy99 <devarunshenoy99@gmail.com>
2026-03-02 13:53:32 +0530

    Update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'df61e6adfb14c6411dd7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '79e4b1d4f865a781b471');
