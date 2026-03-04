7df497b477f1e2255cd7e2ac8134b2032d33b1d7
arunshenoy99 <devarunshenoy99@gmail.com>
2026-03-04 19:59:42 +0530

    Fix legacy footer clickability: give footer higher z-index than content

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'a9358aedfcf58700ce10');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '3ce58adc3560bee711a6');
