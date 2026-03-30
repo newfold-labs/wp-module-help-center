d768b16472d42831a2b0ad0fba4c97250de9f09d
Emanuela Antonina Castorina <emanuela@kream.it>
2026-03-30 09:26:03 +0200

    Clicking on the ? closes or opens the section

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'e4b1c4c20940f8db1693');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '1fc81ab8b7c02a98550c');
